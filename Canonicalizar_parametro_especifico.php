e646b469a129ce3c3846e2c511ae827ae7d0cc25
Your Name <you@example.com>
2021-07-01 20:13:06 +0200

    codigo mejorado

--- Canonicalizar_parametro_especifico.php
+++ Canonicalizar_parametro_especifico.php
@@ -1,6 +1,8 @@
 <?php 
-//con este codigo podemos canonicalizar URLs con parametros especificos. Por ejemplo, los parametros de seguimiento o IDs de usuarios/sesion
-  $urlsinparametros= explode('?', $_SERVER['REQUEST_URI'], 5);
-  isset($_GET["parametro"]) ? print '<link rel="canonical" href="'.$_SERVER["HTTP_HOST"] . $urlsinparametros[0].'">' : '';
-  //hika
-?>
+  $urlsinparametros= explode('?', $_SERVER['REQUEST_URI'], 2);
+  if(isset($_GET["gclid"])){
+    echo '<link rel="canonical" href="'.$_SERVER["HTTP_HOST"] . $urlsinparametros[0].'">';
+  }else{
+    echo '<link rel="canonical" href="'.$_SERVER['HTTP_HOST'].' '.$_SERVER['REQUEST_URI'].'">';
+  }
+  ?>
