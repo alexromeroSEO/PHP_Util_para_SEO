bc7aca5e55f04f7c618976db645e6dbc720143f0
Your Name <you@example.com>
2021-07-01 20:13:46 +0200

    mejorado

--- Canonicalizar_parametro_especifico.php
+++ Canonicalizar_parametro_especifico.php
@@ -1,6 +1,6 @@
 <?php 
-  $urlsinparametros= explode('?', $_SERVER['REQUEST_URI'], 2);
-  if(isset($_GET["gclid"])){
+  $urlsinparametros= explode('?', $_SERVER['REQUEST_URI'], 10);
+  if(isset($_GET["parametro"])){
     echo '<link rel="canonical" href="'.$_SERVER["HTTP_HOST"] . $urlsinparametros[0].'">';
   }else{
     echo '<link rel="canonical" href="'.$_SERVER['HTTP_HOST'].' '.$_SERVER['REQUEST_URI'].'">';
