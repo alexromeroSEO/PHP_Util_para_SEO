75af283dd7e39f0019a277fd98491a9fc67d46a1
Your Name <you@example.com>
2021-07-01 17:27:41 +0200

    añadido codigo

---
+++ Implementar_metarobots_URI_especifica.php
@@ -0,0 +1,10 @@
+<?php 
+//Tienes que implementar una regex que coincida con la URI a la que quieres implementar el meta
+$uri = $_SERVER["REQUEST_URI"];
+if(preg_match("/%RegEx%/", $uri)) 
+{
+ echo '<meta name="robots" content="%lo que quieras%">';
+} else {
+    echo '';
+} 
+?>
