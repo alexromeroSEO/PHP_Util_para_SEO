8b08821b47eec4d0605eee5982aee5e71f9a4af8
Your Name <you@example.com>
2021-07-01 17:23:56 +0200

    c

--- Canonicalizar_parametro_especifico.php
+++ Canonicalizar_parametro_especifico.php
@@ -1,4 +1,4 @@
 <?php 
   $urlsinparametros= explode('?', $_SERVER['REQUEST_URI'], 5);
-  isset($_GET["pparametro"]) ? print '<link rel="canonical" href="'.$_SERVER["HTTP_HOST"] . $urlsinparametros[0].'">' : '';
+  isset($_GET["parametro"]) ? print '<link rel="canonical" href="'.$_SERVER["HTTP_HOST"] . $urlsinparametros[0].'">' : '';
 ?>
