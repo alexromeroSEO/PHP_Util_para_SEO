072bcea2466d832029730bd8415b4681603dfc47
Your Name <you@example.com>
2021-07-04 10:51:57 +0200

    concretado

--- Implementar_metarobots_URI_especifica.php
+++ Implementar_metarobots_URI_especifica.php
@@ -1,5 +1,6 @@
 <?php 
 //Tienes que definir una regex que coincida con la URI a la que quieres implementar el meta
+//Y definir la meta robots concreta que quieres definir (noindex,none,..)
 $uri = $_SERVER["REQUEST_URI"];
 if(preg_match("/%RegEx%/", $uri)) 
 {
